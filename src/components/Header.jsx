fb15f10ff55eae3bdae80864a5aebcd84c7a2107
Noelia <abascalcabreran@gmail.com>
2024-03-29 13:55:12 +0100

    logo as a link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Logo from '../images/Logo.png';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.scss';
 
 function Header() {
   return (
     <header className="header">
       <figure className="header__figure">
-        <img className="header__figure-logo" src={Logo} alt="Logo Rick and Morty" />
+        <Link to="/">
+          <img className="header__figure-logo" src={Logo} alt="Logo Rick and Morty" />
+        </Link>
       </figure>
     </header>
   );
