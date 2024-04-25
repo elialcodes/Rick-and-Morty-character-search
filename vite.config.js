1699670723e6035e6cf9ae2a8dd50b885847d40a
Noelia <abascalcabreran@gmail.com>
2024-04-25 19:37:24 +0200

    json updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/modulo-3-evaluacion-final-elialcodes/',
+  base: '/Rick-and-Morty-character-search/',
 });
